1ddb3d4854553de0f99794f1ddff0bb465fe85bc
ShahrulAzry <s10228942@connect.np.edu.sg>
2023-02-17 17:38:21 +0800

    externalized js and restyled pages

---
+++ script.js
@@ -0,0 +1,10 @@
+// For the sidebar navigation
+function openNav() {
+              document.getElementById("mySidebar").style.width = "500px";
+              document.getElementById("main").style.marginLeft = "250px";
+}
+            
+            function closeNav() {
+              document.getElementById("mySidebar").style.width = "0";
+              document.getElementById("main").style.marginLeft= "0";
+}
